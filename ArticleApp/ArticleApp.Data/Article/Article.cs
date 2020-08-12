fe382b14ca5e2e270f32e2df5e8472f7a95f2c40
Hasan Sefa Yilmaz <hasansefayilmaz@gmail.com>
2020-08-13 01:24:50 +0300

    .

--- ArticleApp/ArticleApp.Data/Article/Article.cs
+++ ArticleApp/ArticleApp.Data/Article/Article.cs
@@ -4,13 +4,13 @@ using System;
 namespace ArticleApp.Data.Article
 {
     public class Article : Entity
-    { 
-        public string Username { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Email { get; set; }
-        public int CreatedBy { get; set; }
-        public int? LastUpdateBy { get; set; }
-        public DateTime? LastUpdateDate { get; set; }
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+        public string Tags { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastUpdateBy { get; set; }
+        public string LastUpdateDate { get; set; }
     }
 }
